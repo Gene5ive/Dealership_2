75cb1f64c7a65d5d16a0db857c2fe8f6b9d949de
Frank Johnson & Jeanette Fairless <student@epicodus.com>
2015-04-29 15:39:40 -0700

    adding spec for path to add dealership page which fails as expected

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -9,4 +9,10 @@ describe('path for dealership index page', {:type => :feature}) do
     click_link('See Dealership List')
     expect(page).to have_content('Dealerships')
   end
+
+  it('user clicks on link for add new dealership') do
+    visit('/')
+    click_link('Add New Dealership')
+    expect(page).to have_content('Add dealerships')
+  end
 end
