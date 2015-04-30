51519c695fd73f21d52aa4d2d3f8ca372f6d4727
Frank Johnson & Jeanette Fairless <student@epicodus.com>
2015-04-29 20:11:51 -0700

    fixing final bug which allows entire app to pass all integration tests.

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -3,52 +3,52 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('path for dealership index page', {:type => :feature}) do
-  it('links user to dealership list page') do
+describe('path for dealership-index page', {:type => :feature}) do
+  it('links user to dealership-list page') do
     visit('/')
     click_link('See Dealership List')
     expect(page).to have_content('Dealerships')
   end
 
-  it('links user to add new dealership page') do
+  it('links user to add-new-dealership page') do
     visit('/')
     click_link('Add New Dealership')
     expect(page).to have_content('Add dealerships')
   end
 end
 
-describe('path for dealership form page', {:type => :feature}) do
-  it('allows user to fill in name of dealership, click add dealership, and link to dealership list page') do
+describe('path for dealership-form page', {:type => :feature}) do
+  it('allows user to fill-in name of dealership, click add dealership, and link to dealership list page') do
     visit('/dealerships/new')
-    fill_in('name', with: "bobs vehicles")
+    fill_in('name', with: "bobs cars")
     click_button('Add Dealership')
     expect(page).to have_content('Dealerships')
   end
 end
 
-describe('path for dealership list page', {:type => :feature}) do
+describe('path for dealership-list page', {:type => :feature}) do
   it('links user to page for list of vehicles in dealership') do
     visit('/dealerships')
-    click_link('bobs vehicles')
+    click_link('bobs cars')
     expect(page).to have_content('Here are all the vehicles in this dealership')
   end
 end
 
-describe('path for vehicles in dealership page', {:type => :feature}) do
-  it('links user to add new vehicle form page') do
+describe('path for vehicles-in-dealership page', {:type => :feature}) do
+  it('links user to add-new-vehicle form page') do
     visit('dealerships/1')
     click_link('Add a new vehicle')
-    expect(page).to have_content('Add a vehicle to bobs vehicles')
+    expect(page).to have_content('Add a vehicle to bobs cars')
   end
 
-  it('links user to dealership index page') do
+  it('links user to dealership-index page') do
     visit('dealerships/1')
     click_link('Return to Main Page')
     expect(page).to have_content('Welcome to Dealership Central')
   end
 end
 
-describe('path for add a vehicle page', {:type => :feature}) do
+describe('path for add-a-vehicle page', {:type => :feature}) do
   it('allows user to fill in make, model, and year of vehicle, click add vehicle, and link to success page') do
     visit('dealerships/1/vehicles/new')
     fill_in('Vehicle\'s make:', with: "Tesla")
@@ -56,5 +56,13 @@ describe('path for add a vehicle page', {:type => :feature}) do
     fill_in('Vehicle\'s year:', with: "2012")
     click_button('Add Vehicle')
     expect(page).to have_content('Success!')
+  end
+end
+
+describe('path for success page', {:type => :feature}) do
+  it('links user to dealership-index page') do
+    visit('dealerships/1')
+    click_link('Return to Main Page')
+    expect(page).to have_content('Welcome to Dealership Central')
   end
 end
